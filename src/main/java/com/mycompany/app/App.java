8bc0e8af8e6cc64293efdc262e938c6591bed73d
Sara Ahbabou <saraa@ip-10-6-18-144.eu-west-1.compute.internal>
2017-06-21 10:05:25 -0700

    adding src files

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,12 @@
+package com.mycompany.app;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {	
+    	if (2 > 1) {
+        System.out.println( "Hello World!" );
+    	}
+    }
+}
